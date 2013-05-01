5829a3587f0f42d7788c394eaf644b6966bf6e0a
Anderson Dadario <anderson@dadario.com.br>
2013-05-01 03:10:47 -0300

    [FEATURE] Mark as paid

---
+++ eventex/subscriptions/tests/test_admin.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+from django.test import TestCase
+from mock import Mock
+from eventex.subscriptions.admin import SubscriptionAdmin, Subscription, admin
+
+class MarkAsPaidTest(TestCase):
+	def setUp(self):
+		# Instancia o Model Admin
+		self.model_admin = SubscriptionAdmin(Subscription, admin.site)
+
+		# Popula o banco
+		Subscription.objects.create(
+			name='Henrique Bastos',
+			cpf='12345678901',
+			email='henrique@bastos.net'
+		)
+
+	def test_has_action(self):
+		'Action mark_as_paid should be installed.'
+		self.assertIn('mark_as_paid', self.model_admin.actions)
+
+	def test_mark_all(self):
+		'Action mark_as_paid shold mark all as paid.'
+		fake_request = Mock()
+		queryset = Subscription.objects.all()
+		self.model_admin.mark_as_paid(fake_request, queryset)
+
+		self.assertEqual(1, Subscription.objects.filter(paid=True).count())
